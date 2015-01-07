bbef0a93ac024bbf2099a5a2b57e64cdec1499fe
Maik Hummel <yo@beevelop.com>
2015-01-07 21:05:27 +0100

    replace buggy slider with normal inputs

--- settings/settings.js
+++ settings/settings.js
@@ -1,16 +1,3 @@
-emp.transRangeFunc = function (x) {
-    return emp.translate('width')+': '+x;
-};
-
-emp.transFunc = function (y) {
-    return emp.translate('height')+': '+y;
-};
-
-emp.transCombinedFunc = function (x,y) {
-    console.log(arguments);
-    return (x/y).toFixed(2);
-};
-
 emp.$watch('settings', function (settings) {
     emp.youtube_url = '//www.youtube';
 
