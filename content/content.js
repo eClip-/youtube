f33b64dc42b23b4df8a677f440cbf032717033d8
Maik Hummel <yo@beevelop.com>
2015-02-24 03:42:32 +0100

    fix settings and styling

--- content/content.js
+++ content/content.js
@@ -1,18 +1,18 @@
-emp.$watch('settings', function (settings) {
+$scope.$watch('settings', function (settings) {
     console.log(settings);
-    emp.youtube_url = '//www.youtube';
+    $scope.youtube_url = '//www.youtube';
 
     if ('privacy_enhanced' in settings && settings.privacy_enhanced)
-        emp.youtube_url += '-nocookie';
+        $scope.youtube_url += '-nocookie';
 
-    emp.youtube_url += '.com/embed/'+settings.video_id+'?';
+    $scope.youtube_url += '.com/embed/'+settings.video_id+'?';
 
     if ('suggested_video' in settings && settings.suggested_video)
-        emp.youtube_url += 'rel=0&amp;';
+        $scope.youtube_url += 'rel=0&amp;';
 
     if ('player_controls' in settings && settings.player_controls)
-        emp.youtube_url += 'controls=0&amp;';
+        $scope.youtube_url += 'controls=0&amp;';
 
     if ('video_title_player_actions' in settings && settings.video_title_player_actions)
-        emp.youtube_url += 'showinfo=0';
+        $scope.youtube_url += 'showinfo=0';
 });
